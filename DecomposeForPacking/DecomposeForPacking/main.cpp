e218cdb2796b5c48b48b844db72aff0ab4031347
TomerGareh <tomergareh@mail.tau.ac.il>
2014-11-04 20:12:49 +0200

    testing project

--- DecomposeForPacking/DecomposeForPacking/main.cpp
+++ DecomposeForPacking/DecomposeForPacking/main.cpp
@@ -7,6 +7,12 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 	cout << "Test proj" << endl;
+	
+	int* arr = new int[5];
+	arr[2] = 5;
+	cout << arr[2];
+	delete[] arr;
+	
 	int x;
 
 	cin >> x;

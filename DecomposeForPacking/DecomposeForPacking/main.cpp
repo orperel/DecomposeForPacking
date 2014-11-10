e2fcc65d21237e2b20b23eb0d7ab5298d96d1a2b
AmitMarcus <amitmarcus@gmail.com>
2014-11-10 22:59:49 +0200

    Bug Fix

--- DecomposeForPacking/DecomposeForPacking/main.cpp
+++ DecomposeForPacking/DecomposeForPacking/main.cpp
@@ -1,91 +1,20 @@
 #pragma once
 
 #include <iostream>
-#include "World.h"
-#include "CImg.h"
-#include "DynamicArray.h"
 #include <string>
+#include "World.h"
 #include "Part.h"
 #include "Decompose.h"
+#include "WorldBuilder.h"
+#include "CImg.h"
 
-using namespace std;
 using namespace cimg_library;
-
-const int BLACK = 255;
-
-WorldPtr imageToWorld(std::string path) {
-	CImg<unsigned char> img(path.c_str());
-
-	//	int width = src.width();
-	//	int height = src.height();
-	//	int depth = src.depth();
-	//	int channels = src.spectrum();
-	//
-	//	cout << "Size of the image: " << width << "x" << height << "\n";
-	//	cout << "Image depth: " << depth << "\n"; //typically equal to 1 when considering usual 2d images
-	//	cout << "Number of channels: " << channels << "\n"; //3 for RGB-coded color images
-	//
-
-	int width = img.width();
-	int height = img.height();
-
-	int min_X = -1;
-	int max_X = -1;
-	int min_Y = -1;
-	int max_Y = -1;
-	int numberOfPoints = 0;
-
-	for (int i = 0; i < width; i++) {
-		for (int j = 0; j < height; j++) {
-			int pixel = (int)img.atXY(i, j);
-
-			if (BLACK != pixel) {
-				numberOfPoints++;
-
-				if (-1 == min_X) {
-					min_X = i;
-				} else {
-					if (i > max_X) {
-						max_X = i;
-					}
-				}
-
-				if (-1 == min_Y) {
-					min_Y = j;
-				} else {
-					if (j > max_Y) {
-						max_Y = j;
-					}
-				}
-			}
-		}
-	}
-
-	cout << width << " - " << height << endl;
-	cout << min_X << " - " << max_X << endl << min_Y << " - " << max_Y << endl;
-
-	BooleanDynamicArrayPtr mat(new DynamicArray<bool>(max_X - min_X + 1, max_Y - min_Y + 1));
-	CImg<unsigned char> bla(max_X - min_X + 1, max_Y - min_Y + 1);
-
-
-	for (int i = min_X; i <= max_X; i++) {
-		for (int j = min_Y; j <= max_Y; j++) {
-			int pixel = (int)img.atXY(i, j);
-
-			(*mat)[i - min_X][j - min_Y] = (BLACK != pixel);
-			(*bla.data(i - min_X, j - min_Y)) = pixel;
-		}
-	}
-
-	//new CImgDisplay(bla);
-
-	return WorldPtr(new World(mat, numberOfPoints));
-}
+using namespace std;
 
 int main(int argc, char *argv[]) {
-	WorldPtr world = imageToWorld("../../obj.bmp");
+	WorldPtr world = WorldBuilder::fromImage("../../obj.bmp");
 
-	PartPtr part(new Part());
+	PartPtr part(new Part(10));
 
 	int lastIndex = part->addPointToRight(0);
 	part->addPointToRight(lastIndex);
@@ -96,6 +25,10 @@ int main(int argc, char *argv[]) {
 	Decompose decompose(world, partList);
 
 	decompose.decompose();
+
+	//shared_ptr<CImg<unsigned char>> img = WorldBuilder::toImage(world);
+
+	//new CImgDisplay(*img);
 
 	cout << "Test proj" << endl;
 	

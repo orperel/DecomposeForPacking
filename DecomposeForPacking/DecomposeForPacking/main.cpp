472f458b1e5fbae4ee460cc1b4435e88bf1b3bff
AmitMarcus <amitmarcus@gmail.com>
2014-11-12 01:22:59 +0200

    Fixed spaces.

--- DecomposeForPacking/DecomposeForPacking/main.cpp
+++ DecomposeForPacking/DecomposeForPacking/main.cpp
@@ -13,7 +13,7 @@
 	#include "Decompose.h"
 	#include "WorldBuilder.h"
 	#include "CImg.h"
-#include "PartBuilder.h"
+	#include "PartBuilder.h"
 
 	using namespace cimg_library;
 	using namespace std;
@@ -30,19 +30,19 @@
 	{
 		WorldPtr world = WorldBuilder::fromImage("../../obj.bmp");
 
-	PartListPtr partList = PartBuilder::buildStandartPartPack(10);
+		PartListPtr partList = PartBuilder::buildStandartPartPack(10);
 
-	Decompose decomposer(world, partList);
+		Decompose decomposer(world, partList);
 
-	decomposer.decompose();
+		decomposer.decompose();
 
-	shared_ptr<CImg<unsigned char>> img = PartBuilder::toImage((*partList)[0]);
+		shared_ptr<CImg<unsigned char>> img = PartBuilder::toImage((*partList)[0]);
 
-	new CImgDisplay(*img);
+		new CImgDisplay(*img);
 
-	shared_ptr<CImg<unsigned char>> img2 = WorldBuilder::toImage(world);
+		shared_ptr<CImg<unsigned char>> img2 = WorldBuilder::toImage(world);
 
-	new CImgDisplay(*img2);
+		new CImgDisplay(*img2);
 
 		cout << "Test proj" << endl;
 

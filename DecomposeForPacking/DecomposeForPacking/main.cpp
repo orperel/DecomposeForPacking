4c506bdc6998c7202433a7e5d337893bd9894a85
orperel <or_perel@hotmail.com>
2014-11-11 02:42:18 +0200

    Intergrating tests with main, connecting visitor

--- DecomposeForPacking/DecomposeForPacking/main.cpp
+++ DecomposeForPacking/DecomposeForPacking/main.cpp
@@ -1,38 +1,63 @@
 #pragma once
 
-#include <iostream>
-#include <string>
-#include "World.h"
-#include "Part.h"
-#include "Decompose.h"
-#include "WorldBuilder.h"
-#include "CImg.h"
+// Uncomment this line to run tester instead
+//#define RUN_TESTS 
 
-using namespace cimg_library;
-using namespace std;
+#ifdef RUN_TESTS
+	#include "dlxSolverTester.h"
+#else
+	#include <iostream>
+	#include <string>
+	#include "World.h"
+	#include "Part.h"
+	#include "Decompose.h"
+	#include "WorldBuilder.h"
+	#include "CImg.h"
+
+	using namespace cimg_library;
+	using namespace std;
+#endif
+
+#ifdef RUN_TESTS
+	void runTests()
+	{
+		dlxSolverTester tester;
+		tester.runTests();
+	}
+#else
+	void runProd()
+	{
+		WorldPtr world = WorldBuilder::fromImage("../../obj.bmp");
+
+		PartPtr part(new Part(10));
+
+		int lastIndex = part->addPointToRight(0);
+		part->addPointToRight(lastIndex);
+
+		PartList partList;
+		partList.push_back(part);
+
+		Decompose decompose(world, partList);
+
+		decompose.decompose();
+
+		//shared_ptr<CImg<unsigned char>> img = WorldBuilder::toImage(world);
+
+		//new CImgDisplay(*img);
+
+		cout << "Test proj" << endl;
+
+		int x;
+
+		cin >> x;
+	}
+#endif
 
 int main(int argc, char *argv[]) {
-	WorldPtr world = WorldBuilder::fromImage("../../obj.bmp");
 
-	PartPtr part(new Part(10));
-
-	int lastIndex = part->addPointToRight(0);
-	part->addPointToRight(lastIndex);
-
-	PartList partList;
-	partList.push_back(part);
-
-	Decompose decompose(world, partList);
-
-	decompose.decompose();
-
-	//shared_ptr<CImg<unsigned char>> img = WorldBuilder::toImage(world);
-
-	//new CImgDisplay(*img);
-
-	cout << "Test proj" << endl;
-	
-	int x;
-
-	cin >> x;
+#ifdef RUN_TESTS
+	runTests();
+#else
+	runProd();
+#endif
 }

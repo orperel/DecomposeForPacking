9d6befe4395e43691b0e4a8194b98185b3f21406
TomerGareh <tomergareh@mail.tau.ac.il>
2014-11-16 10:07:51 +0200

    AlgoXResult update

--- DecomposeForPacking/DecomposeForPacking/AlgoXResult.h
+++ DecomposeForPacking/DecomposeForPacking/AlgoXResult.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include <unordered_map>
+#include <map>
 #include <tuple>
 #include "Part.h"
 #include "DLXSolver.h"
 
 using std::tuple;
-using std::unordered_map;
+using std::map;
 
-typedef unordered_map<Part, int> PartsCount;
+typedef map<Part, int> PartsCount;
 typedef vector<tuple<PartOrientation, int>> PartLocationList;
 
 class AlgoXResult
